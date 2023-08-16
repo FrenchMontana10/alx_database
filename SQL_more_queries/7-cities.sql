4a43e244f7624d4b6c99ac0bd4d870bb9e47d51e
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-16 01:15:57 +0000

    eighth commit

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,23 @@
--- Lists all cities of CA in the database hbtn_0d_usa.
--- Results are ordered by ascending cities.id.
-SELECT `id`, `name`
-  FROM `cities`
- WHERE `state_id` IN
-       (SELECT `id`
-	        FROM `states`
-	       WHERE `name` = "California")
-ORDER BY `id`;
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database
+USE hbtn_0d_usa;
+
+-- Create the cities table if it doesn't exist
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
+
+-- Insert data into the cities table
+INSERT INTO cities (state_id, name) VALUES (1, "San Francisco");
+
+-- Select data from the cities table
+SELECT * FROM cities;
+
+-- Attempt to insert data that violates the foreign key constraint
+-- This will result in an error
+-- INSERT INTO cities (state_id, name) VALUES (10, "Paris");
